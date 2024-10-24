f42b09e4bb6f0dcb07ecf8bae396bb14ae2448f1
Cepri Damiri <damiricepri127@gmail.com>
2024-10-24 14:27:34 +0000

    all request method done

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,7 @@ module.exports = function (app) {
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
-      if (!title) return res.send("Missing required field title");
+      if (!title) return res.send("missing required field title");
       const data = new modelDB({
         title,
         commentcount: 0
@@ -39,32 +39,66 @@ module.exports = function (app) {
             title: user.title
           });
         }).catch((err)=>{
-          console.error("Error cuk", err);
+          console.error("Error saving user:", err);
           return;
         });
     })
     
-    .delete(function(req, res){
+    .delete(async function(req, res){
       //if successful response will be 'complete delete successful'
+      await modelDB.deleteMany({});
+      return res.send("complete delete successful");
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res){
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try{
+        const data = await modelDB.findById(bookid);
+        if (!data) return res.send("no books exists");
+        return res.json(data);
+      }catch(error){
+        return "its error";
+      }
     })
     
-    .post(function(req, res){
+    .post(async function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) return res.send("missing required field comment");
+      const checkdata = await modelDB.findById(bookid);
+      if(!checkdata) return res.send("no book exists");
+      try{
+        await modelDB.updateOne({
+          _id: bookid,
+        },
+        {
+          $push: {comment: comment},
+          $inc: {commentcount: 1},
+        },
+      );
+      const data = await modelDB.findById(bookid);
+      return res.json(data);
+      }catch(error){
+        console.log(error);
+      }
     })
     
-    .delete(function(req, res){
+    .delete(async function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      try{
+        const data = await modelDB.findById(bookid);
+        if (!data) return res.send("no book exists");
+        await modelDB.deleteOne({_id: bookid});
+        return res.send("delete successful");
+      }catch(error){
+        
+      }
     });
   
 };
