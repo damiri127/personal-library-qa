8b9f032f2b55b546cd66a5aeba7d79a75922bf35
Cepri Damiri <damiricepri127@gmail.com>
2024-10-24 13:52:21 +0000

    Get and Post Method done

--- routes/api.js
+++ routes/api.js
@@ -19,13 +19,29 @@ module.exports = function (app) {
         const data = await modelDB.find();
         res.json(data);
       }catch(err){
-        console.log(err);
+        console.log('Error nih cuy!');
       }
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (!title) return res.send("Missing required field title");
+      const data = new modelDB({
+        title,
+        commentcount: 0
+      });
+      data
+        .save()
+        .then((user)=>{
+          return res.json({
+            _id: user._id,
+            title: user.title
+          });
+        }).catch((err)=>{
+          console.error("Error cuk", err);
+          return;
+        });
     })
     
     .delete(function(req, res){
